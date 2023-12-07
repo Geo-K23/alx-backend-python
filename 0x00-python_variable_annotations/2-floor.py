0baebe86eb48980d52dbebc3ff4dc4976ceda162
George Kazibwe <gkazibwe@gmail.com>
2023-12-07 09:17:40 -0800

    Tasks 1 to 5

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor
+"""
+
+
+def floor(a: float) -> int:
+    """
+    Return the floor of a floating-point number
+    """
+    return int(a)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a floating-point number to its string representation
+    """
+    return str(n)
